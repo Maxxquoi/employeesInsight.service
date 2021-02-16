045f4aec073abd2d7e65fcda7697c619fb7a368e
Emmanuel Massaquoi <EmmanuelMassaquoi@protonmail.com>
2021-02-15 19:16:32 -0700

    controller setup

--- employeesInsight.webapi/v1/Controllers/Member/EmployeesController.cs
+++ employeesInsight.webapi/v1/Controllers/Member/EmployeesController.cs
@@ -9,31 +9,10 @@ namespace employeesInsight.webapi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class EmployeesController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        public EmployeesController()
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
         }
     }
 }
